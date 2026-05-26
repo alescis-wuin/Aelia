5edf9febb03aeaef7ea8ba02b35e53a7c312417e
Alexis <alexis.guinot@onsiea.com>
2026-05-26 20:27:57 +0200

    fix(map-view): remove unavailable jdk.jsobject module dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,13 +3,10 @@ module fr.alescis.aelia {
     requires javafx.controls;
     requires javafx.graphics;
     requires javafx.web;
-    requires jdk.jsobject;
     requires atlantafx.base;
 
     exports fr.alescis.aelia;
     exports fr.alescis.aelia.model;
     exports fr.alescis.aelia.provider;
     exports fr.alescis.aelia.service;
-
-    opens fr.alescis.aelia.ui.components to javafx.web;
 }
