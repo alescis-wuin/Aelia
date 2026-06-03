9f98902eec88ef3fb8b3c70ec16a922c3b31c334
Alexis <alexis.guinot@onsiea.com>
2026-06-03 13:15:29 +0200

    feature(domain) : add account type taxonomy
    Defines the account categories needed by the first wealth-tracking domain model.
    
    Refs: phase-1

---
+++ wealth-tracker/backend/domain/src/main/java/fr/alescis/aelia/wealth/domain/model/AccountType.java
@@ -0,0 +1,13 @@
+package fr.alescis.aelia.wealth.domain.model;
+
+public enum AccountType {
+    CURRENT_ACCOUNT,
+    REMUNERATED_CURRENT_ACCOUNT,
+    SAVINGS_ACCOUNT,
+    SECURITIES_ACCOUNT,
+    PEA,
+    CRYPTO_EXCHANGE,
+    CRYPTO_WALLET,
+    CASHBACK_ACCOUNT,
+    OTHER
+}
