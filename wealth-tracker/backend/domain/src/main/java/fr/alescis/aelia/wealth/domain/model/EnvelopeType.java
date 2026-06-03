e9686c6b4c6311fb5d80e9819ffc5d5515bf6d6d
Alexis <alexis.guinot@onsiea.com>
2026-06-03 13:18:50 +0200

    feature(domain) : add wealth envelope taxonomy
    Defines the first reporting envelope categories used to group accounts and positions independently from account implementation details.
    
    Refs: phase-1

---
+++ wealth-tracker/backend/domain/src/main/java/fr/alescis/aelia/wealth/domain/model/EnvelopeType.java
@@ -0,0 +1,14 @@
+package fr.alescis.aelia.wealth.domain.model;
+
+public enum EnvelopeType {
+    NONE,
+    CURRENT_ACCOUNT,
+    REGULATED_SAVINGS,
+    BANK_SAVINGS,
+    PEA,
+    CTO,
+    CRYPTO,
+    PRIVATE_ASSET,
+    CASHBACK,
+    OTHER
+}
