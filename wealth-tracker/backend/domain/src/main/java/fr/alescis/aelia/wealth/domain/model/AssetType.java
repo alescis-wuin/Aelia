d8064086f509df049380cb4a369bf115e9407788
Alexis <alexis.guinot@onsiea.com>
2026-06-03 13:19:35 +0200

    feature(domain) : add asset type taxonomy
    Defines the first asset categories for fiat, crypto, listed securities, bonds, funds and private assets.
    
    Refs: phase-1

---
+++ wealth-tracker/backend/domain/src/main/java/fr/alescis/aelia/wealth/domain/model/AssetType.java
@@ -0,0 +1,13 @@
+package fr.alescis.aelia.wealth.domain.model;
+
+public enum AssetType {
+    FIAT_CURRENCY,
+    CRYPTOCURRENCY,
+    STOCK,
+    ETF,
+    BOND,
+    FUND,
+    PRIVATE_ASSET,
+    CASHBACK,
+    OTHER
+}
