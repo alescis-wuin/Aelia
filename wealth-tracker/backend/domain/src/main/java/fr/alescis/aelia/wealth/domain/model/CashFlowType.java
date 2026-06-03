d51c2e7fca19c2f8dd20d48f319c01e0349763dc
Alexis <alexis.guinot@onsiea.com>
2026-06-03 13:20:03 +0200

    feature(domain) : add cash flow taxonomy
    Defines the first cash-flow categories needed to separate external capital movements from income, costs and corrections.
    
    Refs: phase-1

---
+++ wealth-tracker/backend/domain/src/main/java/fr/alescis/aelia/wealth/domain/model/CashFlowType.java
@@ -0,0 +1,15 @@
+package fr.alescis.aelia.wealth.domain.model;
+
+public enum CashFlowType {
+    DEPOSIT,
+    WITHDRAWAL,
+    TRANSFER_IN,
+    TRANSFER_OUT,
+    INTEREST,
+    DIVIDEND,
+    FEE,
+    TAX,
+    CASHBACK,
+    REWARD,
+    CORRECTION
+}
