db23197c903084f2de076c78d131e50c5ef75679
Alexis <alexis.guinot@onsiea.com>
2026-06-03 13:23:31 +0200

    feature(domain) : add valuation method classification
    Defines the first valuation methods so snapshots can distinguish observed balances, manual estimates, imports and calculated values.
    
    Refs: phase-1

---
+++ wealth-tracker/backend/domain/src/main/java/fr/alescis/aelia/wealth/domain/model/ValuationMethod.java
@@ -0,0 +1,10 @@
+package fr.alescis.aelia.wealth.domain.model;
+
+public enum ValuationMethod {
+    OBSERVED_BALANCE,
+    MANUAL_ESTIMATE,
+    IMPORTED_VALUE,
+    MARKET_PRICE,
+    FX_CONVERTED,
+    CALCULATED
+}
